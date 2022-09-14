f702a63378718ae1286f1a19c9c43e2b1cbc7117
Ruslan Trashchotkin <ruslantrashchotkin@Ruslans-MacBook-Pro.local>
2022-09-15 01:14:20 +0400

    c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { AfterViewInit, Component, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import * as twModule from '../lib/index';
+import { ActivatedRoute } from '@angular/router';
+import { filter, map, pluck, switchMap } from 'rxjs';
 
 const tw = twModule.default;
 
@@ -14,6 +16,7 @@ export class AppComponent implements AfterViewInit {
   constructor(
     private http: HttpClient,
     private el: ElementRef,
+    private route: ActivatedRoute,
   ) {
     console.log(tw);
   }
@@ -21,8 +24,14 @@ export class AppComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
 
-
-    this.http.get<any>('http://dev.teleporta.me/token')
+    this.route.queryParams
+    .pipe(
+      map((params) => {
+        return params?.identity;
+      }),
+      filter(Boolean),
+      switchMap((identity) => this.http.get<any>(`http://dev.teleporta.me/token?identity=${identity}`)),
+    )
     .subscribe(({ token }) => {
       console.log('token', token);
       tw.createLocalTracks({
@@ -41,14 +50,32 @@ export class AppComponent implements AfterViewInit {
         )
       })
       .then((room) => {
+        window['twRoom'] = room;
         console.log(`Successfully joined a Room: ${room}`);
         room.on('participantConnected', participant => {
           console.log(`A remote Participant connected: ${participant}`);
-          participant.tracks.forEach(t => this.addMediaEl(t));
+          participant.tracks.forEach(publication => {
+            if (publication.isSubscribed) {
+              const track: any = publication.track;
+              this.addMediaEl(track);
+            }
+          });
+        
+          participant.on('trackSubscribed', (track: any) => {
+            this.addMediaEl(track);
+          });
         });
 
         room.participants.forEach(p => {
-          p.tracks.forEach(t => this.addMediaEl(t));
+          console.log(p);
+          p.on('trackSubscribed', (track: any) => {
+            this.addMediaEl(track);
+          });
+          p.tracks.forEach(p => {
+            if (p.track) {
+              this.addMediaEl(p.track);
+            }
+          });
         });
       })
       .catch((err) => {
